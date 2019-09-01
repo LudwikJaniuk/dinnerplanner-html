212d6008f89be3f98d3b98b27950747714b29be9
unnadig <tjeriph@gmail.com>
2019-09-01 21:48:57 +0200

    cosmetic fix

--- src/model/dinnerModel.js
+++ src/model/dinnerModel.js
@@ -3,28 +3,23 @@ class DinnerModel {
 
   constructor() {
     this.dishes = dishesConst;
-
-    //TODO Lab 0
-    // implement the data structure that will hold number of guests
-    // and selected dishes for the dinner menu
     this.numberOfGuests = 4;
     this.menu = [];
 
   }
 
   setNumberOfGuests(num) {
-    //TODO Lab 0
       this.numberOfGuests = num;
+      
   }
 
   getNumberOfGuests() {
-    //TODO Lab 0
       return this.numberOfGuests;
+      
   }
 
   //Returns the dish that is on the menu for selected type 
   getSelectedDish(type) {
-    //TODO Lab 0
       let menu = getFullMenu();
       for (menuDish in menu) {
           if (menuDish.type === type) {
@@ -38,24 +33,23 @@ class DinnerModel {
 
   //Returns all the dishes on the menu.
   getFullMenu() {
-    //TODO Lab 0
       return this.menu;
+      
   }
 
   //Returns all ingredients for all the dishes on the menu.
   getAllIngredients() {
-    //TODO Lab 0
       let menu = getFullMenu;
       let ingredients = [];
       for (menuDish in menu) {
           ingredients = ingredients.concat(menuDish.ingredients);
       }
       return ingredients;
+      
   }
 
   //Returns the total price of the menu (all the ingredients multiplied by number of guests).
   getTotalMenuPrice() {
-    //TODO Lab 0
       let ingredients = getAllIngredients();
       let numberOfGuests = getNumberOfGuests();
       let sum = 0;
@@ -64,12 +58,12 @@ class DinnerModel {
       }
       sum *= numberOfGuests;
       return sum;
+      
   }
 
   //Adds the passed dish to the menu. If the dish of that type already exists on the menu
   //it is removed from the menu and the new one added.
   addDishToMenu(id) {
-    //TODO Lab 0
     let dish = getDish(id);
     if (dish === undefined) {
         break;
@@ -88,7 +82,6 @@ class DinnerModel {
 
   //Removes dish from menu
   removeDishFromMenu(id) {
-    //TODO Lab 0
     let menu = getFullMenu();
     for (i=0; i<menu.length; i++) {
         let menuDish = menu[i];
